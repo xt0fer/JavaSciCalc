013a00d3b318aefa7f881a0d3026925a58478aa8
Bobbi-Z <85585077+Bobbi-Z@users.noreply.github.com>
2021-06-27 19:15:39 -0400

    the struggle is real

--- src/main/java/com/zipcodewilmington/scientificcalculator/CalculatorDisplay.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/CalculatorDisplay.java
@@ -2,9 +2,9 @@ package com.zipcodewilmington.scientificcalculator;
 
 public class CalculatorDisplay {
 
-    public void currentEngineReturn(){
 
-        switch ( new CalculatorEngine()){
+
+
 
 
     }
